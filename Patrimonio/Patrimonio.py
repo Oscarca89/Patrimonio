cd2dfdd5815d7009ce30effea8f8866f3e684d75
Oscarca89 <oscarcamilo.24@gmail.com>
2024-11-09 20:30:54 -0600

    Update Patrimonio.py

--- Patrimonio/Patrimonio.py
+++ Patrimonio/Patrimonio.py
@@ -31,8 +31,8 @@ def cargar_procesar_archivo():
                     st.stop()
 
             # Mantener solo las columnas de interés
-            columnas_interes = ["Soc.",	 "Activo fijo",	"SNº",	"Clase",	"Ce.coste",	"Div.",	"Cta.CAP AV01",	"Cta.CAP AV03",	"Cta.CAP AV50",	"Fe.capit.",	"Denominación del activo fijo",	 "VU",	 "/",	"VTA",	 "/"	"Val.adq. 	           Amo acum. 	       
-            Val.cont. 	           Val.adq. 	           Amo acum. 	         Val.cont. 	           Val.adq. 	           Amo acum. 	          Val.cont. 	Orden inv.	Elemento PEP	CeBe	Número de serie
+            columnas_interes = ["Soc.",	 "Activo fijo",	"SNº",	"Clase",	"Ce.coste",	"Div.",	"Cta.CAP AV01",	"Cta.CAP AV03",	"Cta.CAP AV50",	"Fe.capit.", "Denominación del activo fijo", "VU",	"/", "VTA",	 "/",	"Val.adq.",  "Amo acum.", 	       
+            "Val.cont."  "Val.adq."  "Amo acum.", "Val.cont.", 	  "Val.adq.",  "Amo acum.", "Val.cont.", "Orden inv.",	"Elemento PEP",	"CeBe",	"Número de serie"
 ]
             dataframe_bat = dataframe_bat[[col for col in columnas_interes if col in dataframe_bat.columns]]
 
